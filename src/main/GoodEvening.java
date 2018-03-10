87ba767e64b8b146640e171d0448385bbff0bea4
sheroziy <sheroziy_uz@yahoo.com>
2018-03-10 12:21:20 -0500

    added print statement

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -3,6 +3,8 @@ package main;
 public class GoodEvening {
 	public static void main(String[] args) {
 		System.out.println("Good Evening Team!");
+		
+		System.out.println("My name is Sheroziy");
 	}
 
 }
